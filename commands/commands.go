c3a54c421b60c01edbb0f9d27cfbbc28429e0dc4
Kristen Sala <salakristen1@gmail.com>
2023-10-08 00:17:51 +0300

    send message base

--- commands/commands.go
+++ commands/commands.go
@@ -1,6 +1,13 @@
 package commands
 
-import "fmt"
+import (
+	"encoding/xml"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/tiaguinho/gosoap"
+)
 
 const (
     Ecp = "ecp"
@@ -11,6 +18,18 @@ type Mades struct {
     Ecp string
     Edx string
 }
+
+type SendMessageResponseResult struct {
+    MessageId string `xml:"messageID"`
+}
+
+type SendMessageResponse struct {
+    SendMessageResponseResult string `xml:"SendMessageResponseResult"`
+}
+
+var (
+    sendMessageResponse SendMessageResponse
+)
 
 func (m Mades) getUrl(command string) string {
     switch command {
@@ -24,9 +43,38 @@ func (m Mades) getUrl(command string) string {
 }
 
 func (m Mades) SendMessage(command string, message string) {
-    //wsdlUrl := w.getUrl(command)
+    wsdlUrl := m.getUrl(command)
 
-    fmt.Println(message)
+    httpClient := &http.Client{}
+    soapClient, err := gosoap.SoapClient(wsdlUrl, httpClient)
+    if err != nil {
+        log.Fatalf("Soap client error: %s", err)
+        return
+    }
 
+    params := gosoap.Params{
+        "message": gosoap.Params{
+            "content": "message here", //todo: has to be byte array
+            "baMessageID": "", // optional
+        },
+        "conversationID": "",
+    }
+
+    res, err := soapClient.Call("SendMessage", params)
+    if err != nil {
+        log.Fatalf("Could not set the message. Error: %s", err)
+        return
+    }
+
+    res.Unmarshal(&sendMessageResponse)
+    result := SendMessageResponseResult{}
+
+    err = xml.Unmarshal([]byte(sendMessageResponse.SendMessageResponseResult), &result)
+    if err != nil {
+        log.Fatalf("Could not unmarchal response. Error: %s", err)
+        return
+    }
+
+    fmt.Println(result)
 }
 
--- go.mod
+++ go.mod
@@ -5,6 +5,9 @@ go 1.21.2
 require (
 	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/tiaguinho/gosoap v1.4.4 // indirect
 	github.com/urfave/cli/v2 v2.25.7 // indirect
 	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
+	golang.org/x/net v0.0.0-20191209160850-c0dbc17a3553 // indirect
+	golang.org/x/text v0.3.2 // indirect
 )
